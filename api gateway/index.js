0f1fef5d1aa63f21c9b72c3f465a71a0d2a6050e
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2026-08-05 22:54:43 +0545

    fixing the port

--- api gateway/index.js
+++ api gateway/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import proxy from "express-http-proxy"
 import cookieParser from "cookie-parser"
+import morgan from "morgan"
 
 const app = express()
 
@@ -16,21 +17,21 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(cookieParser())
-
+app.use(morgan("dev"))
 
 // request routing
-app.use("/auth", proxy("http://localhost:8001"))
-app.use("/projects", proxy("http://localhost:8002"))
-app.use("/tasks", proxy("http://localhost:8003"))
-app.use("/notification", proxy("http://localhost:8004"))
-
+app.use("/api/auth", proxy("http://localhost:8001"))
+app.use("/api/user", proxy("http://localhost:8001"))
+app.use("/api/projects", proxy("http://localhost:8002"))
+app.use("/api/tasks", proxy("http://localhost:8003"))
+app.use("/api/notification", proxy("http://localhost:8004"))
 
 // directly listens to the user/FE for the request as they only know this port
-app.listen(8000, (err)=>{
+app.listen(8010, (err)=>{
     if(err){
         console.log("an error has occurred in api gateway")
     }
     else{
-        console.log("The gate way has started listening")
+        console.log("The API gateway is on ✅.")
     }
 })
