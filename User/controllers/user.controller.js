1a28fbd84c3610fc877a5a6cf5e37785f7a42254
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2026-08-06 19:48:10 +0545

    user controller done

--- User/controllers/user.controller.js
+++ User/controllers/user.controller.js
@@ -1,48 +1,85 @@
+import express from "express"
+import {authenticate} from "../middlewares/authenticate.js"
+import {authorize} from "../middlewares/authorize.js"
+import { UserModel } from "../models/user.model.js"
 
 
-const getTasks = (req, res, next) => {
-    try{
-
-    }
-    catch(err){
-        next(err)
-    }
-}
-
-const getTask = (req, res, next) => {
-    try{
-
-    }
-    catch(err){ 
-        next(err)
-    }
-}
-
-const changeTask = (req, res, next) => {
-    try{
-
-    }
-    catch(err){
-        next(err)
-    }
-}
-
-const assignTask = (req, res, next) => {
-    try{
-
-    }
-    catch(err){
-        next(err)
-    }
+export const userRoutes = async(app, channel) => {
+    app.get("/profile", authenticate, authorize("developer", "pm"), getProfile)
+    app.get("/",authenticate, authorize("pm", "developer"), getAllUsers)
+    app.get("/:id", authenticate, authorize("pm", "developer"), getUser)
+    app.delete("/:id", authenticate, authorize("pm", "developer"), deleteUser)
+    app.delete("/", deleteAllUsers)
 }
 
 const getProfile = (req, res, next) => {
     try{
-
+        res.json(req.user)
     }
     catch(err){
         next(err)
     }
 }
 
-export {getTasks, getTask, changeTask, getProfile}
+
+const getAllUsers = async(req, res, next) => {
+    try{
+        const users = await UserModel.find({ role : "developer"}).select("-password")
+        if(users.length == 0){
+            res.json({
+                message : "no developers"
+            })
+        }
+        res.json(users)
+    }
+    catch(err){
+        next(err)
+    }
+}
+
+
+const getUser = async(req, res, next) => {
+    try{
+        const user_id = req.params.id
+        const user = await UserModel.findById(user_id).select("-password")
+        if(!user){
+            const err = new Error("no user found with such id")
+            err.status = 404
+            throw err
+        }
+        res.json(user)
+    }
+    catch(err){
+        next(err)
+    }
+}
+
+
+const deleteUser = async(req, res, next) => {
+    try{
+        console.log(req.params)
+        const user = await UserModel.findByIdAndDelete(req.params.id)
+        if(!user){
+            const err = new Error("no user found")
+            throw err
+        }
+        res.json({
+            message : "user deleted"
+        })
+    }
+    catch(err){
+        next(err)
+    }
+}
+
+
+const deleteAllUsers = async(req, res, next) => {
+    const users = await UserModel.find({})
+    await UserModel.deleteMany({
+        _id : { $exists : true}
+        })
+        res.json({
+            users : users,
+            message : "deleted all the users"
+        })
+}
