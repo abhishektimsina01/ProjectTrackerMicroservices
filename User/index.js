ee72847e4b49690c27030a96fa11d6c3efb0d49e
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2026-08-06 12:15:21 +0545

    app configuration

--- User/index.js
+++ User/index.js
@@ -2,18 +2,19 @@ import express from "express"
 import { notFound, errorHandler } from "./middlewares/errorHandler.js"
 import {authRouter} from "./routes/auth.route.js"
 import {userRouter} from "./routes/user.route.js"
+import morgan from "morgan"
+import { authRoutes } from "./controllers/auth.controller.js"
+import { userRoutes } from "./controllers/user.controller.js"
 
-const app = express()
 
-app.use((req, res, next) => {
-    console.log("this is the middleware of the Auth service")
-    next()
-})
-
-app.use("/api/auth", authRouter)
-app.use("/api/user", userRouter)
-
-app.use(notFound)
-app.use(errorHandler)
-
-export {app}
+export const appConfiguration = (app, channel) => {
+    app.use((req, res, next) => {
+        console.log("this is the middleware of the Auth service")
+        next()
+    })
+    app.use(morgan())
+    authRoutes(app, channel)
+    userRoutes(app, channel)
+    app.use(notFound)
+    app.use(errorHandler)
+}
--- User/server.js
+++ User/server.js
@@ -1,25 +1,31 @@
+import express from "express"
 import {connectDB} from "./db.config.js"
 import dotenv from "dotenv"
-import {app} from "./index.js"
-
+import { createChannel } from "./utils/rabbitMQ.js"
+import { appConfiguration } from "./index.js"
 dotenv.config()
 
-const startServer  = (app) => {
-    connectDB(process.env.url).then(() => {
-        console.log("app is listening")
+
+const startServer = async => {
+    try{
+        const app = express()
+        await connectDB(process.env.url)
+        // const channel = await createChannel()
+        appConfiguration(app, channel = null)
         app.listen(8001,(err) => {
         if(err){
             console.log("error in the server")
         }
         else{
-            console.log("Auth service in on✅")
+            console.log("Auth and User services are on✅")
             }
         })
-    })
-    .catch((err) => {
+    }
+    catch(err){
         console.log(err.name)
         console.log(err.message)
         console.log("couldnot connect with the database")
-    })
+    }
 }
-startServer(app)
+
+startServer()
